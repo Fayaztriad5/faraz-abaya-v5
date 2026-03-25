7183aedd328fd3a23d404de487a2b0e64bb47eac
mac5 <fayaztriad5@gmail.com>
2026-03-26 01:29:26 +0530

    Update orderApi.js

--- src/utils/orderApi.js
+++ src/utils/orderApi.js
@@ -1,4 +1,4 @@
-const API_BASE = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+const API_BASE = process.env.REACT_APP_API_BASE_URL || "";
 
 export async function saveOrderToSheet(payload) {
   const res = await fetch(`${API_BASE}/api/orders`, {
